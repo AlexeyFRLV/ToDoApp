aab4e5a4217ecaa256ef8571569a9f7b226b2870
Alexey Frolov <foritlearning@gmail.com>
2021-03-25 01:05:12 +0300

    Создаём модель данных

---
+++ Models/ToDoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    class ToDoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+
+    }
+}
